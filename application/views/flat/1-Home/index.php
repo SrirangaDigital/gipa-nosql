ca26828dd0d9b8db22a4600fd6a0952c47c83049
suresh07 <soori_rozee@yahoo.com>
2018-03-09 13:43:14 +0530

    Home page modified, removed old project data

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,106 +1,71 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <div class="container-fluid" id="home">
-    <div class="row first-row">
-        <div class="col-md-12 clear-paddings">
-            <div class="fixOverlayDivHome gap-above">
-                <div class="OverlayTextMain">
-                    <div class="mainpage">
-                        <h2>IIT Madras Heritage Centre</h2>
-                        <p>The Heritage Centre was formally inaugurated by Dr. Arcot Ramachandran, former Director of IIT Madras on 3rd March 2006. The Centre is located on the ground floor of the administration building. The actual idea of a Heritage Centre was mooted in the year 2000 and it became a reality due to the efforts of Dr. Ajit Kumar Kolar and his team. The Centre is a repository of material of heritage value and historical significance of various facets of the Institute.</p>
-                    </div>
-                </div>
-            </div>
+  <div class="row first-row">
+    <div class="col-md-12 clear-paddings">
+      <div class="fixOverlayDivHome gap-above">
+        <div class="OverlayTextMain">
+          <div class="mainpage">
+            <h2>The Gokhale Institute of Public Affairs (GIPA)</h2>
+            <p>The Gokhale Institute of Public Affairs (GIPA) is an independent, non-party and non communal organization endeavouring to serve as a center for the education of the public for democratic citizenship. It seeks to Co-operate with, and seeks Co-operation from the Government and all public institutions in the country.</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
-<div class="container" id="about">
-    <ul class="timeline">
-        <li>
-          <div class="timeline-badge">1956</div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p>Visit of Prime Minister Jawaharlal Nehru to West Germany and the offer made to him by the Government of the Federal Republic of Germany to assist in the setting up of a higher technological institute in India.</p>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-badge">1958</div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p>The signing of the first Indo-German agreement in Bonn, West Germany for the establishment of the Indian Institute of Technology at Madras.</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="timeline-badge">1959</div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p>Formal inauguration of the Institute by Prof.Humayun Kabir, Union Minister for Scientific Research and Cultural Affairs, Dr.A.Lakshmanaswamy Mudaliar assumes office as the first Chairman of the Board of Governors of the Institute. Prof.B.Sengupto assumes office as the first Director of the Institute.</p>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-badge">1961</div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p>Declaration of the Institute by the Parliament of India as an institution of National importance.</p>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="timeline-badge">1962</div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p>Visit to the Institute by Dr.Heinrich Lubke, President of the Federal Republic of Germany and the unveiling of a tablet symbolising the Indo-German technical assistance programme.</p>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-badge">1967</div>
-          <div class="timeline-panel">
-            <div class="timeline-body">
-              <p>Dr. A. Ramachandran takes over as Director from Prof. B. Sengupto.</p>
-            </div>
-          </div>
-        </li>
-    </ul>
+<div class="container" id="background">
+  <div class="row gap-above">
+    <div class="col-md-12">
+      <h2>Background</h2>
+    </div>
+  </div>
+  <p>The death of Gopal Krishna Gokhale on 19th February 1915 evoked an outburst of grief and sense of loss throught the country. Leaders like Balagangadhar Tilak and Mahatma Gandhi paid rich tributes to the departed leader. There was a desire every where to make an organised effort to serve the country after the example of gokhale.</p>
+  <h4><strong>1915-1941</strong></h4>
+  <p>In Bangalore a largely attended public meeting was held on 25th February 1915 at which Sir M.Visveswaraya, then Dewan of Mysore, spoke in appreciation of Gokhale's personality and his work. Inspired by the speeches of national leaders and others, a small group of young men decided to start an association called The Mysore Social Service League. Among those who took active interest in that endeavour may be mentioned D.V.Gundappa, K.S.Krishna Iyer. A.R.Nageshwara Iyer, N.Narasimha Murthy, M.G.Varadachar, C.S.Anantha Padmanabha Iyer, S.Surya Prakash, Mokshagundam Krishna Murthy, Belur Srinivasa Iyengar, B.V.Subba Rao and K.Bheema Rao. It is this small group which decided to invite Mahatma Gandhi to Bangalore in May 1915 and requested him to bless their idea. Gandhiji was touring in South India at that time, and had proposed to visit Madras. A telegram was sent to him and the invitation was accepted</p>
+  <p id="right"><a href="<?=BASE_URL?>Background">More ...</a></p>
 </div>
 <div class="container-fluid stats" id="collection">
-    <div class="row clear-margins">
-        <div class="col-md-12">
-            <h1>Gallery</h1>
-            <ul class="list-inline">
-                <li class="stat-elem">
-                    <a href="<?=BASE_URL?>listing/categories/Ebook?select=Collection">
-                        <h2><i class="fa fa-book"></i></h2>
-                        <p>E-Books</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
+  <div class="row clear-margins">
+    <div class="col-md-12">
+      <h1>Gallery</h1>
+      <ul class="list-inline">
+        <li class="stat-elem">
+          <a href="<?=BASE_URL?>listing/categories/Ebook?select=Collection">
+            <h2><i class="fa fa-book"></i></h2>
+            <p>E-Books</p>
+          </a>
+        </li>
+        <li class="stat-elem">
+          <a href="javascript:void(0);">
+            <h2><i class="fa fa-archive"></i></h2>
+            <p alt="Coming Soon">Archives</p>
+          </a>
+        </li>
+      </ul>
     </div>
+  </div>
 </div>
 <div class="container-fluid physical" id="physical">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Physical Space</h1>
-        </div>
+  <!-- <div class="row">
+    <div class="col-md-12">
+      <h1>Physical Space</h1>
     </div>
-    <div class="row">
-        <div class="col-md-4">
-            <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/ps1.JPG">
-            <h2>Centrally located</h2>
-            <p>The Heritage Centre is located in the ground floor of the Administration Block, right next to Gajendra Circle. It occupies the space where the old manual telephone exchange was situated.</p>
-        </div>
-        <div class="col-md-4">
-            <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/ps2.JPG">
-            <h2>Pictorial account</h2>
-            <p>The display at the Heritage Centre provides a pictorial account of the events that led to the founding of IIT Madras, student life in the early years of the institute, the development of technological and other facilities and visits by distinguished people. The display also encompasses the campus and distinuished alumni.</p>
-        </div>
-        <div class="col-md-4">
-            <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/ps3.JPG">
-            <h2>Variety of artefacts</h2>
-            <p>Also on display are a variety of artefacts including the foundation stone laid at the inauguration of the institute in 1959, a life size oil painting of Prof. B. Sengupto, the first Director of IIT Madras, and a replica of the Inter-IIT Sports Trophy.</p>
-        </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/ps1.JPG">
+      <h2>Centrally located</h2>
+      <p>The Heritage Centre is located in the ground floor of the Administration Block, right next to Gajendra Circle. It occupies the space where the old manual telephone exchange was situated.</p>
     </div>
+    <div class="col-md-4">
+      <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/ps2.JPG">
+      <h2>Pictorial account</h2>
+      <p>The display at the Heritage Centre provides a pictorial account of the events that led to the founding of IIT Madras, student life in the early years of the institute, the development of technological and other facilities and visits by distinguished people. The display also encompasses the campus and distinuished alumni.</p>
+    </div>
+    <div class="col-md-4">
+      <img class="img-responsive" src="<?=PUBLIC_URL?>images/stock/ps3.JPG">
+      <h2>Variety of artefacts</h2>
+      <p>Also on display are a variety of artefacts including the foundation stone laid at the inauguration of the institute in 1959, a life size oil painting of Prof. B. Sengupto, the first Director of IIT Madras, and a replica of the Inter-IIT Sports Trophy.</p>
+    </div>
+  </div> -->
 </div>
